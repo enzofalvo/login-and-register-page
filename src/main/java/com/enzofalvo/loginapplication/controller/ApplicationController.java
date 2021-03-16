35b3e4049c197247a749c4bf38ef90849e16c953
enzofalvo <enzo.fvo@gmail.com>
2021-03-16 14:54:16 -0300

    Deploy para o Heroku

--- src/main/java/com/enzofalvo/loginapplication/controller/ApplicationController.java
+++ src/main/java/com/enzofalvo/loginapplication/controller/ApplicationController.java
@@ -2,16 +2,12 @@ package com.enzofalvo.loginapplication.controller;
 
 import com.enzofalvo.loginapplication.model.User;
 import com.enzofalvo.loginapplication.service.UserService;
-import java.util.ResourceBundle.Control;
-import javax.persistence.Cache;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
